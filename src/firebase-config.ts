f3360d90f4637557c5601ea830418592321cd642
rj-freaker <rupeshkumar706011@gmail.com>
2024-06-06 18:07:35 +0530

    added env

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,12 +1,12 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhJrP_u3LErkZ5XTbSWXQt09ZWhrt6Pbk",
-    authDomain: "breatheesg-4e08c.firebaseapp.com",
-    projectId: "breatheesg-4e08c",
-    storageBucket: "breatheesg-4e08c.appspot.com",
-    messagingSenderId: "203813709543",
-    appId: "1:203813709543:web:c80bc4dbcc4e698a5ce442",
-    measurementId: "G-Q0HKNBM2PM"
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 
